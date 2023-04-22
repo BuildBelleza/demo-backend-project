5b44d651259f156a06794109b864f06c895b6153
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-04-22 12:38:10 -0400

    add views directory

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 
+const es6Renderer = require('express-es6-template-engine');
+
 const express = require('express');
 
 const server = express();
+
+server.engine('html', es6Renderer);
+server.set('views', 'views');
+server.set('view engine', 'html');
+
+server.get('/', (req, res) => {
+    res.render('index');
+});   // this is the home page
 
 const PORT = process.env.PORT || 8080;
 
